c9258d80ad5716ee26bd55831282dbb71dc925ac
Dasha Savenko <savenkodasa772@gmail.com>
2024-04-13 13:01:16 +0300

    Removed duplicate page for chapter

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import AddBook from "../presentation/pages/AddBook";
 import BookPage from "../presentation/pages/BookPage";
-import Editor from "../presentation/pages/Editor";
 import Login from "../presentation/pages/Login";
 import MainPage from "../presentation/pages/MainPage";
 import NotFoundPage from "../presentation/pages/NotFoundPage";
@@ -30,10 +29,6 @@ const router = createBrowserRouter([
   {
     path: "/addbook",
     element: <AddBook />,
-  },
-  {
-    path: "/editor",
-    element: <Editor />,
   },
   {
     path: "/profile",
