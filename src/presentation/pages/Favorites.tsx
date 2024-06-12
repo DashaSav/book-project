427cb9ea50b2d3c9f42ff6f468e912ba0825d688
Dasha Savenko <savenkodasa772@gmail.com>
2024-06-13 02:07:19 +0300

    fixed favorites rating

--- src/presentation/pages/Favorites.tsx
+++ src/presentation/pages/Favorites.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 import DefaultPageLayout from "./DefaultPage";
-import { getFavoriteBooks } from "../../data/apiService";
+import { getBookRating, getFavoriteBooks } from "../../data/apiService";
 import Book from "../components/Book";
 
 export default function Favorites() {
   const [books, setBooks] = useState<DBBook[]>([]);
+  const [ratings, setRatings] = useState<Map<string, number | null>>(new Map());
 
   useEffect(() => {
     getFavoriteBooks()
-      .then((books) => setBooks(books))
+      .then((books) => {
+        setBooks(books);
+        getRatings();
+      })
       .catch((e) => console.log(e));
   }, []);
 
@@ -18,10 +22,25 @@ export default function Favorites() {
         <h4 className="mb-2 mt-2">Мое избранное:</h4>
         <div className="flex-cont">
           {books.map((book) => (
-            <Book book={book} isFavorite={true} />
+            <Book
+              book={book}
+              isFavorite={true}
+              rating={ratings.get(book._id)}
+            />
           ))}
         </div>
       </DefaultPageLayout>
     </>
   );
+
+  async function getRatings() {
+    const newRatings = new Map<string, number | null>();
+
+    for (const b of books) {
+      const r = await getBookRating(b._id);
+      newRatings.set(b._id, r?.rating ?? null);
+    }
+
+    setRatings(newRatings);
+  }
 }
